42c98d33ac3815097791ce2bbc6f232acd800908
Your Name <email@example.com>
2019-07-20 17:03:21 +0800

    aaaaaaaa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <FooterGuide/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import FooterGuide from './components/FooterGuide/index'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    FooterGuide
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='stylus'>
+  #app
+    width 100%
+    position absolute
+    top 0
+    bottom 0
+    background #000
+    font-family: PingFangSC-Light,helvetica,'Heiti SC'
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
+import './cube-ui'
 import App from './App.vue'
+import 'amfe-flexible'
+
+//导入cube
+import Cube from 'cube-ui'
+
+//路由
+import router from './router/index'
+
+
+Vue.use(Cube)
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
+
